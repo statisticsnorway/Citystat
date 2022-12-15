8505c46b48c61a660900e00b09bede67f83171be
Stian Nergård <snergaard18@gmail.com>
2022-12-15 16:27:50 +0100

    loops for table production

--- DE1_tables/Functions.r
+++ DE1_tables/Functions.r
@@ -1,3 +1,43 @@
+###############################################################
+# Functions made for production of demograhpy tables (DE1_2022)
+# Project: CITYSTATISTICS
+# Author: Stian Nergård
+###############################################################
+
+# Summarise rows function:
+# Takes a variable to filter on and what to filter to as inputs,
+#    and outputs a summary table.
+# Eg. sum_rows(Alder, "F00-04")
+
+sum_rows <- function(filter_by, filter_to) {
+    
+    x <- deparse(substitute(filter_by)) # creates a string
+
+    df_DE1 %>%
+        filter(df_DE1[x] == filter_to) %>%
+        group_by(code) %>%
+        summarise(Value = sum(value))
+}
+
+# Filter rows function:
+# Takes two strings to specify age group and gender filter as inputs,
+#    and outputs a summary table.
+# Eg. filter_rows("F")
+
+filter_rows <- function(Alder_filter, Kjonn_filter) {
+
+    df_DE1 %>%
+        filter(Alder == Alder_filter,
+               Kjonn == Kjonn_filter) %>%
+        select(code, value) %>%
+        rename(Value = value)
+}
+
+# Table formatting function:
+# Takes a summary table (as produced by the other functions),
+#    and formats correctly for Eurostat export.
+# Eg. table_formatting(DE1001V_2022)
+
 table_formatting <- function(sum_table) {
 
     year <- substr(deparse(substitute(sum_table)), 9, 12)
@@ -18,23 +58,4 @@ table_formatting <- function(sum_table) {
         arrange(City_code)
     
     return(sum_table)
-}
-
-sum_rows <- function(filter_by, filter_to) {
-    
-    x <- deparse(substitute(filter_by))
-
-    df_DE1 %>%
-        filter(df_DE1[x] == filter_to) %>%
-        group_by(code) %>%
-        summarise(Value = sum(value))
-}
-
-sum_rows_2 <- function(Alder_filter, Kjonn_filter) {
-
-    df_DE1 %>%
-        filter(Alder == Alder_filter,
-               Kjonn == Kjonn_filter) %>%
-        select(code, value) %>%
-        rename(Value = value)
 }

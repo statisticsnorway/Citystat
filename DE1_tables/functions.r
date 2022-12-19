cf59a146ad6745cda8560e686c6818ed54a23116
Stian Nergård <snergaard18@gmail.com>
2022-12-19 13:38:55 +0100

    processing updates

--- DE1_tables/functions.r
+++ DE1_tables/functions.r
@@ -39,11 +39,14 @@ filter_rows <- function(Alder_filter, Kjonn_filter) {
 # Eg. table_formatting(DE1001V_2022)
 
 table_formatting <- function(sum_table) {
+    
+    string <- sum_table
+    object <- get(sum_table)
 
-    year <- substr(deparse(substitute(sum_table)), 9, 12)
-    variable_name <- substr(deparse(substitute(sum_table)), 1, 7)
+    year <- substr(sum_table, 9, 12)
+    variable_name <- substr(sum_table, 1, 7)
 
-    sum_table <- sum_table %>%
+    object <- object %>%
         mutate(Reference_year = year,
                Variable_code = variable_name,
                Flags = "",
@@ -57,5 +60,5 @@ table_formatting <- function(sum_table) {
         rename(City_code = code) %>%
         arrange(City_code)
     
-    return(sum_table)
+    return(object)
 }
